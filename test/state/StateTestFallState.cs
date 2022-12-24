d8fccd3f1350c52c6de596e9b1952eb7473c0bd6
Ethan Tucker <ectucker1@gmail.com>
2022-12-23 20:25:22 -0600

    Add demo scene for state machines

---
+++ test/state/StateTestFallState.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+public class StateTestFallState : StateNode<StateTestPlayer>
+{
+    public StateTestFallState(StateTestPlayer player) : base(player)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        StateOwner.Modulate = Colors.Orange;
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+     
+        if (StateOwner.IsOnFloor())
+        {
+            StateOwner.MovementState.PopState();
+        }
+    }
+}
---
+++ test/state/StateTestJumpState.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+
+public class StateTestJumpState : StateNode<StateTestPlayer>
+{
+    public StateTestJumpState(StateTestPlayer player) : base(player)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        StateOwner.Modulate = Colors.Red;
+        StateOwner.Velocity.y = -200;
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (StateOwner.Velocity.y > 0)
+        {
+            StateOwner.MovementState.ReplaceState(new StateTestFallState(StateOwner));
+        }
+    }
+}
