8de2026bae39cfb0e71885d5d6db40c4d1a5d50c
Ella Tucker <ectucker1@gmail.com>
2026-03-24 22:28:05 -0500

    Use ResourceLoader instead of DirAccess to list test scenes - DirAccess on resources doesn't work in exports

--- test/generate_test_buttons.gd
+++ test/generate_test_buttons.gd
@@ -4,29 +4,18 @@ const TEST_BUTTON_SCENE := preload("res://test/test_scene_button.gd")
 
 
 func _ready() -> void:
-	var test_dir := DirAccess.open("res://test/")
 	var scene_paths: Array[String] = []
-	_scan_for_test_scenes(test_dir, "res://test/", true, scene_paths)
+	for path in ResourceLoader.list_directory("res://test/"):
+		if not path.ends_with("/"):
+			continue
+		for file in ResourceLoader.list_directory("res://test/" + path):
+			if file.ends_with(".tscn") and file.contains("test"):
+				scene_paths.append("res://test/" + path + file)
 	scene_paths.sort_custom(func(a: String, b: String) -> bool:
 		return a.get_file() < b.get_file()
 	)
 	for scene_path in scene_paths:
 		_add_test_button(scene_path)
-
-
-func _scan_for_test_scenes(dir: DirAccess, path: String, is_root: bool, results: Array[String]) -> void:
-	dir.list_dir_begin()
-	var file_name := dir.get_next()
-	while file_name != "":
-		var full_path := path + file_name
-		if dir.current_is_dir():
-			var sub_dir := DirAccess.open(full_path)
-			if sub_dir:
-				_scan_for_test_scenes(sub_dir, full_path + "/", false, results)
-		elif file_name.ends_with(".tscn") and file_name.contains("test") and not is_root:
-			results.append(full_path)
-		file_name = dir.get_next()
-	dir.list_dir_end()
 
 
 func _add_test_button(scene_path: String) -> void:
